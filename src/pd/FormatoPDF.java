f6a6f0a364b9df15be33ca069f8f77ae7f431a07
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-16 12:33:58 -0500

    FormatoPDF, solo se leera el formato

--- src/pd/FormatoPDF.java
+++ src/pd/FormatoPDF.java
@@ -4,23 +4,78 @@
  */
 package pd;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageDestination;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineNode;
 
 /**
  *
  * @author Rojas Piña Efrain Ulises <al2172001457@azc.uam.mx>
  */
-public class FormatoPDF {
+public class FormatoPDF implements AbstracFormato {
+
    private String nombreFormato;
-   private ArrayList<Seccion> formatoSeccion;
+   private ArrayList<Seccion> bookmarkSeccion;
    private Seccion seccion;
+   private int id;
 
    public FormatoPDF(String nombreFormato) {
       this.nombreFormato = nombreFormato;
-      formatoSeccion = new ArrayList<>();
+      bookmarkSeccion = new ArrayList<>();
+      id = 0;
+   }
+
+   @Override
+   public ArrayList<Seccion> leerFormato() {
+      try {
+         PDDocument documentPDF = PDDocument.load(new File(nombreFormato));
+         PDDocumentOutline outline = documentPDF.getDocumentCatalog().
+                 getDocumentOutline();
+         formatoSecciones(documentPDF, outline);
+         documentPDF.close();
+         return this.bookmarkSeccion;
+      } catch (IOException e) {
+         System.err.println("Error al leer o escribir en el archivo con la ruta especificada");
+      } catch (NullPointerException e) {
+         System.err.println("Error al leer los bookmarks del archivo");
+      }
+      return null;
+   }
+
+   public void formatoSecciones(PDDocument document, PDOutlineNode bookmark) throws IOException {
+      PDOutlineItem current = bookmark.getFirstChild();
+      while (current != null) {
+         ++id;
+         seccion = new Seccion(id, current.getTitle());
+         bookmarkSeccion.add(seccion);
+         System.out.println(id + " " + current.getTitle());
+         formatoSecciones(document, current);
+         current = current.getNextSibling();
+      }
    }
    
-   
+   @Override
+   public ArrayList<Seccion> getBookmarkSeccion() {
+      return bookmarkSeccion;
+   }
+
+   @Override
+   public void setBookmarkSeccion (ArrayList<Seccion> bookmarkSeccion) {
+      this.bookmarkSeccion = bookmarkSeccion;
+   }
+
+   @Override
+   public void getBookmarks() {
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
+
+   public void array(){
+      System.out.println(" " + bookmarkSeccion.toString());
+   }
    
 }
