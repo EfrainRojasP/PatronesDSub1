676f35f37cafb5cd3d2d8f1b8bb9bbfeaf567933
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-16 11:55:31 -0500

    Se agrego mas elementos a seccion

--- src/pd/Seccion.java
+++ src/pd/Seccion.java
@@ -14,6 +14,64 @@ public class Seccion {
    private String nombre;
    private int numPalabras;
    private String texto;
-   
-   
+
+   public Seccion(int id) {
+      this(id, "NO DEFINIDA");
+   }
+
+   public Seccion(int id, String nombre) {
+      this(id, nombre, 0, "");
+   }
+
+   public Seccion(int id, String nombre, int numPalabras, String texto) {
+      this.id = id;
+      this.nombre = nombre;
+      this.numPalabras = numPalabras;
+      this.texto = texto;
+   }
+
+   public Seccion() {
+
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getNombre() {
+      return nombre;
+   }
+
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
+
+   public int getNumPalabras() {
+      return numPalabras;
+   }
+
+   public void setNumPalabras(int numPalabras) {
+      this.numPalabras = numPalabras;
+   }
+
+   public String getTexto() {
+      return texto;
+   }
+
+   public void setTexto(String texto) {
+      this.texto = texto;
+   }
+
+   @Override
+   public String toString() {
+      return "Seccion{" + "id=" + id
+              + ",\nnombre=" + nombre
+              + ",\nnumPalabras=" + numPalabras
+              + ",\ntexto=" + texto + '}';
+   }
+
 }
