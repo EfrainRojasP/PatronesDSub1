128ef19a9d7288a410a9848ed89df63e4959e630
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-17 07:04:49 -0500

    Formato Listo WORD

--- src/pd/FormatoWord.java
+++ src/pd/FormatoWord.java
@@ -4,10 +4,68 @@
  */
 package pd;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
 /**
  *
  * @author Rojas Piña Efrain Ulises <al2172001457@azc.uam.mx>
  */
-public class FormatoWord {
-   
+public class FormatoWord implements AbstracFormato {
+
+   private String nombreFormato;
+   private ArrayList<Seccion> bookmarkSeccion;
+   private Seccion seccion;
+
+   public FormatoWord(String nombreFormato) {
+      this.nombreFormato = nombreFormato;
+      bookmarkSeccion = new ArrayList<>();
+   }
+
+   @Override
+   public ArrayList<Seccion> leerFormato() {
+      try {
+         XWPFDocument documento = new XWPFDocument(new FileInputStream(nombreFormato));
+         XWPFWordExtractor xwpfWordExtractor = new XWPFWordExtractor(documento);
+         List<XWPFParagraph> paragraphs = documento.getParagraphs();
+         
+         int idSeccion = 1;
+         for (XWPFParagraph para : paragraphs) {
+           if (para.getStyleID() != null && (para.getStyleID().compareTo("Ttulo") == 0
+                    || para.getStyleID().compareTo("Ttulo1") == 0
+                    || para.getStyleID().compareTo("Ttulo2") == 0
+                    || para.getStyleID().compareTo("Ttulo3") == 0
+                    || para.getStyleID().compareTo("Subttulo") == 0)) {
+               seccion = new Seccion(idSeccion, para.getText());
+               bookmarkSeccion.add(seccion);
+               ++idSeccion;
+            } 
+         }
+         return  this.bookmarkSeccion;
+      } catch (FileNotFoundException ex) {
+         Logger.getLogger(FormatoWord.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (IOException ex) {
+         Logger.getLogger(FormatoWord.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      return null;
+   }
+
+   @Override
+   public ArrayList<Seccion> getBookmarkSeccion() {
+      return bookmarkSeccion;
+   }
+
+   @Override
+   public void setBookmarkSeccion(ArrayList<Seccion> formatoSeccion) {
+      this.bookmarkSeccion = bookmarkSeccion;
+   }
+
 }
