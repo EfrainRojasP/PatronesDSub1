f9bd50998312fb4c2e17252907168ab798b1b366
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-17 06:05:17 -0500

    Construccion del reporte

--- src/pd/ReportePDF.java
+++ src/pd/ReportePDF.java
@@ -4,6 +4,11 @@
  */
 package pd;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Table;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 /**
@@ -19,7 +24,6 @@ public class ReportePDF {
    }
    
    
-   
    public void comparacion(){
       for(Seccion seccion : seccionPA){
          if(seccion.isCumplido() && seccion.getNumPalabras() == 0){
@@ -28,8 +32,37 @@ public class ReportePDF {
       }
    }
    
-   public void construirReporte(){
-   
+   public void construirReporte() throws FileNotFoundException{
+      String file
+              = "addingTableToPDF.pdf";
+
+      // Step-1 Creating a PdfDocument object
+      PdfDocument pdfDoc
+              = new PdfDocument(new PdfWriter(file));
+
+      // Step-2 Creating a Document object
+      Document doc = new Document(pdfDoc);
+
+      // Step-3 Creating a table
+      Table table = new Table(2);
+
+      // Step-4 Adding cells to the table}
+      table.addCell("Formato");
+      table.addCell("Cumplido");
+      for (Seccion seccion : seccionPA) {
+         table.addCell(seccion.getNombre());
+         if (seccion.isCumplido()) {
+            table.addCell("Si");
+         } else {
+            table.addCell("No");
+         }
+      }
+
+      // Step-6 Adding Table to document
+      doc.add(table);
+
+      // Step-7 Closing the document
+      doc.close();
    }
    
 }
