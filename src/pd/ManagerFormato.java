4d60c7eed9a5f9c0b5c15746767247e3b15c5bfb
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-18 01:01:43 -0500

    Manager formato

--- src/pd/ManagerFormato.java
+++ src/pd/ManagerFormato.java
@@ -8,6 +8,51 @@ package pd;
  *
  * @author Rojas Piña Efrain Ulises <al2172001457@azc.uam.mx>
  */
-public class ManagerArchivo {
+public class ManagerFormato {
+   private FormatoPDF formatoPDF;
+   private FormatoWord formatoWord;
+   private String nombreFormato;
+   
+   public ManagerFormato() {
+   }
+
+   public ManagerFormato(String nombreFormato) {
+      this.nombreFormato = nombreFormato;
+   }
+   
+   public void leerFormatoPDF(){
+      formatoPDF = new FormatoPDF(nombreFormato);
+      formatoPDF.leerFormato();
+   }
+   
+   public void leerFormatoWord(){
+      formatoWord = new FormatoWord(nombreFormato);
+      formatoWord.leerFormato();
+   }
+   
+   public String getNombreFormato() {
+      return nombreFormato;
+   }
+
+   public void setNombreFormato(String nombreFormato) {
+      this.nombreFormato = nombreFormato;
+   }
+
+   public FormatoPDF getFormatoPDF() {
+      return formatoPDF;
+   }
+
+   public void setFormatoPDF(FormatoPDF formatoPDF) {
+      this.formatoPDF = formatoPDF;
+   }
+
+   public FormatoWord getFormatoWord() {
+      return formatoWord;
+   }
+
+   public void setFormatoWord(FormatoWord formatoWord) {
+      this.formatoWord = formatoWord;
+   }
+   
    
 }
