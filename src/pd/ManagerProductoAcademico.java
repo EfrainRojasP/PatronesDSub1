88a095943bb81c03246ef72d81dbf51bc609c15b
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-18 01:59:21 -0500

    Se añadio get para los bookmarks del PA

--- src/pd/ManagerProductoAcademico.java
+++ src/pd/ManagerProductoAcademico.java
@@ -4,10 +4,68 @@
  */
 package pd;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Rojas Piña Efrain Ulises <al2172001457@azc.uam.mx>
  */
 public class ManagerProductoAcademico {
+   private ProductoPDF productoPDF;
+   private ProductoWord productoWord;
+   private String nombrePA;
+
+   public ManagerProductoAcademico() {
+   }
+
+   public ManagerProductoAcademico(String nombrePA) {
+      this.nombrePA = nombrePA;
+   }
+   
+   public void leerProductoAcademicoPDF(ArrayList<Seccion> bookMarksFormato){
+      productoPDF = new ProductoPDF(nombrePA, bookMarksFormato);
+      productoPDF.leerPA();
+      productoPDF.construirSecciones();
+   }   
+   
+   public void leerProductoAcademicoWord(ArrayList<Seccion> bookMarksFormato){
+      productoWord = new ProductoWord(nombrePA, bookMarksFormato);
+      productoWord.leerPA();
+      productoWord.construirSecciones();
+   }
+
+   public ProductoPDF getProductoPDF() {
+      return productoPDF;
+   }
+
+   public void setProductoPDF(ProductoPDF productoPDF) {
+      this.productoPDF = productoPDF;
+   }
+
+   public ProductoWord getProductoWord() {
+      return productoWord;
+   }
+
+   public void setProductoWord(ProductoWord productoWord) {
+      this.productoWord = productoWord;
+   }
+
+   public String getNombrePA() {
+      return nombrePA;
+   }
+
+   public void setNombrePA(String nombrePA) {
+      this.nombrePA = nombrePA;
+   }
+   
+   public ArrayList<Seccion> getBookMarksPDF(){
+      return productoPDF.getBookmarkSeccionFormato();
+   }
+   
+   public ArrayList<Seccion> getBookMarksWord(){
+      return productoWord.getBookmarkSeccionFormato();
+   }
+   
+   
    
 }
