26b35b006a8d50a9318b9c0f0c8f41f4a7e4e057
EfrainRojasP <105468821+EfrainRojasP@users.noreply.github.com>
2022-05-17 23:02:07 -0500

    Implementa AbstractReporte

--- src/pd/ReportePDF.java
+++ src/pd/ReportePDF.java
@@ -10,17 +10,25 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Table;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Rojas Piña Efrain Ulises <al2172001457@azc.uam.mx>
  */
-public class ReportePDF {
+public class ReportePDF implements AbstracRepote{
    //private Seccion seccion;
    private ArrayList<Seccion> seccionPA;
+   private String nombrePA;
 
    public ReportePDF(ArrayList<Seccion> seccionPA) {
       this.seccionPA = seccionPA;
+   }
+
+   public ReportePDF(ArrayList<Seccion> seccionPA, String nombrePA) {
+      this.seccionPA = seccionPA;
+      this.nombrePA = nombrePA;
    }
    
    
@@ -32,37 +40,56 @@ public class ReportePDF {
       }
    }
    
-   public void construirReporte() throws FileNotFoundException{
-      String file
-              = "addingTableToPDF.pdf";
-
-      // Step-1 Creating a PdfDocument object
-      PdfDocument pdfDoc
-              = new PdfDocument(new PdfWriter(file));
-
-      // Step-2 Creating a Document object
-      Document doc = new Document(pdfDoc);
-
-      // Step-3 Creating a table
-      Table table = new Table(2);
-
-      // Step-4 Adding cells to the table}
-      table.addCell("Formato");
-      table.addCell("Cumplido");
-      for (Seccion seccion : seccionPA) {
-         table.addCell(seccion.getNombre());
-         if (seccion.isCumplido()) {
-            table.addCell("Si");
-         } else {
-            table.addCell("No");
+   @Override
+   public void construirReporte(){
+      try {
+         String file = nombreReporte();
+         
+         
+         //1. Creacion del PDF
+         PdfDocument pdfDoc
+                 = new PdfDocument(new PdfWriter(file));
+         
+         //2. Creacion del docuemnto como objeto
+         Document doc = new Document(pdfDoc);
+         
+         //3. Creando tabla
+         // Table(2) -> Indica que la tabla tendra dos columnas
+         Table table = new Table(2);
+         
+         //4. Añadimos las celas a la tabla
+         table.addCell("Formato");
+         table.addCell("Cumplido");
+         for (Seccion seccion : seccionPA) {
+            table.addCell(seccion.getNombre());
+            if (seccion.isCumplido()) {
+               table.addCell("Si");
+            } else {
+               table.addCell("No");
+            }
          }
+         
+         //6. Añadimos la tabla al docuemento
+         doc.add(table);
+         
+         //7. Cerramos el documento
+         doc.close();
+      } catch (FileNotFoundException ex) {
+         Logger.getLogger(ReportePDF.class.getName()).log(Level.SEVERE, null, ex);
       }
-
-      // Step-6 Adding Table to document
-      doc.add(table);
-
-      // Step-7 Closing the document
-      doc.close();
+   }
+   
+   public String nombreReporte(){
+      String nombrePDF = "";
+      if(nombrePA.lastIndexOf(".docx") > 0){
+         nombrePDF = "ReporteCumplidos_".concat(nombrePA.substring(0, 
+                 nombrePA.lastIndexOf(".docx")));
+         nombrePDF += ".pdf";
+         return nombrePDF;
+      } else {
+         nombrePDF = "ReporteCumplidos_".concat(nombrePDF);
+         return nombrePDF;
+      }
    }
    
 }
